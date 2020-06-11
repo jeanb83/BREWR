56942e6fc70b6986863dc719c10ab3ba312744ba
Jean Brisou <jean.brisou@kedgebs.com>
2020-06-11 10:22:16 +0200

    home page and fonts colors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def home
+  end
+
+  def configure_permitted_parameters
+  # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname])
+
+  # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end

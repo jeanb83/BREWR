767b8aa13886ed88412fc291ea199c1b1b3a5261
celine turchetti <celine.turchetti@wanadoo.fr>
2020-06-11 15:38:28 +0200

    constant type vote model done

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,10 @@
 class Vote < ApplicationRecord
   belongs_to :event_membership
+  TYPES = ["Bistros", "Breakfast & Brunch", "Burgers", "Chinese", "French", "Indian", "Italian", "Japanese", "Korean", "Pizza", "Sushi", "Thai", "Vegan"]
+
+  def self.get_vote_types
+    TYPES
+  end
 end
+
+

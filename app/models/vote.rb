2384cd647ffed5b723dd0ed343cd121e1056e66c
celine turchetti <celine.turchetti@wanadoo.fr>
2020-06-11 15:54:34 +0200

    last modif vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,10 +1,13 @@
 class Vote < ApplicationRecord
+  VOTE_TYPES = ["Bistros", "Breakfast & Brunch", "Burgers", "Chinese",
+                 "French", "Indian", "Italian", "Japanese", "Korean",
+                 "Pizza", "Sushi", "Thai", "Vegan"]
+
   belongs_to :event_membership
-  TYPES = ["Bistros", "Breakfast & Brunch", "Burgers", "Chinese", "French", "Indian", "Italian", "Japanese", "Korean", "Pizza", "Sushi", "Thai", "Vegan"]
+  validates :value, inclusion: { in: VOTE_TYPES }
+  validates :value, inclusion: { in: [nil, -100, 0, 1] }
 
   def self.get_vote_types
-    TYPES
+    return VOTE_TYPES
   end
 end
-
-

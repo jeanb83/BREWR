0f5bc6c48a9904cb1d0fb6b709a5412a7240b224
Maxime Cochini <maxime.cochini@gmail.com>
2020-06-15 11:05:19 +0200

    Font added to custom fonts folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,33 +26,11 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+import { avatarClick } from '../components/avatar_click';
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  listenAvatarClicks();
-});
-
-
-let avatars = Array.from(document.querySelectorAll('.avatars'));
-// Add class active to last clicked avatar
-const avatarClick = (event) => {
-  // Remove "active" class for every items in the list
-  avatars.forEach(node => {
-    node.classList.remove('active');
-  });
-  // Add "active" class for last clicked avatar
-  event.currentTarget.classList.add('active');
-  // Get dataset value of last clicked avatar
-  const avatarFile = event.currentTarget.dataset.avatar;
-  // Set hidden form field value to that value
-  document.getElementById('avatar').value = avatarFile;
-  console.log(avatarFile);
-}
-
-// Listen to clicks on avatars
-const listenAvatarClicks = () => avatars.forEach(node => {
-  node.addEventListener('click', avatarClick);
-  console.log("Loaded.");
+  avatarClick();
 });

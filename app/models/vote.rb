68070f3ea1bdebb10e8338d6762a8c94e94a9292
Maxime Cochini <maxime.cochini@gmail.com>
2020-06-15 11:26:26 +0200

    After_create for votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,10 +4,40 @@ class Vote < ApplicationRecord
                  "Pizza", "Sushi", "Thai", "Vegan"]
 
   belongs_to :event_membership
+
+  has_one :event, through: :event_membership
+
   validates :taste, inclusion: { in: VOTE_TASTES }
   validates :like, inclusion: { in: [nil, -100, 0, 1] }
+
+  after_create :compile_votes
 
   def self.get_vote_tastes
     return VOTE_TASTES
   end
+
+  private
+
+  def compile_votes
+    # Get the vote's event
+    @event = self.event
+    # Check if there's enough votes
+    if @event.votes.count >= @event.users.count
+      # Initialize compilation as a hash
+      @compiled_votes = {}
+      # Initialize all possible tastes to 0 for compilation
+      VOTE_TASTES.each do |taste|
+        @compiled_votes[taste] = 0
+      end
+      # Compile the votes
+      @event.votes.each do |vote|
+        @compiled_votes[vote.taste] += vote.like
+      end
+      # Get the bigger vote and store in events term column
+      @event.term = @compiled_votes.max_by { |taste, like| like }[0]
+      # Save
+      @event.save
+    end
+  end
+  
 end

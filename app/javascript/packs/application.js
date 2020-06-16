5ca000511591b04bd25cedd2b74a8e64e9b054f1
Jean Brisou <jean.brisou@kedgebs.com>
2020-06-16 09:36:25 +0200

    added Countdown + cards to the dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -56,3 +56,21 @@ const listenAvatarClicks = () => avatars.forEach(node => {
   node.addEventListener('click', avatarClick);
   console.log("Loaded.");
 });
+
+// CountDown for votes
+var count=30;
+
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+
+  document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling//Do code for showing the number of seconds here
+}

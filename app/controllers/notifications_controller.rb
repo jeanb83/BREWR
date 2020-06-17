f0e30e3c97e33595ad99368dbc3c585ff3201100
Jean Brisou <jean.brisou@kedgebs.com>
2020-06-17 17:19:08 +0200

    modif on waiting page + index events

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,5 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.order(created_at: :desc)
   end
 end
